1ecc076887f01cb76bbfaf711de97d279fe916b4
Rex Mudanya <rex@swiftlabs.ai>
2020-12-16 12:26:57 +0300

    breakdown of functions and wanted features

---
+++ methods.py
@@ -0,0 +1,27 @@
+# todo: load data method
+# todo: data prep 
+# todo: create models
+# todo: score models
+# todo: create submission csv for each model
+# todo: add hyperparameter optimization to encoder and models
+# todo: pick best method based on RMSE
+
+'''
+data prep:
+1. scale data
+2. encode data (auto encoder)
+'''
+
+'''
+create models:
+1. add hyperparameter optimization
+2. output score, RMSE
+3. create submission script for each model
+'''
+
+'''
+create submission:
+1. add bools for :
+- scaled only
+- scale + encoded
+'''
